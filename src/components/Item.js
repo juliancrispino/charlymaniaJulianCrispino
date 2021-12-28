13fcc84694273ffbaa8ec64d3a79069b67150f69
Julian <crispinojulian@hotmail.com>
2021-12-28 19:21:19 -0300

    arreglo desafio 6

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,31 +1,15 @@
 import ItemCount from "./ItemCount"
 
-const items = [
-    { id: 1, nombre: "Yendo de la cama al living", año: 1982, portada: "/img/yendo de la cama al living.jpg", precio: 800, stock: 5 },
-    { id: 2, nombre: "Clics modernos", año: 1983, portada: "/img/clics modernos.jpg", precio: 1000, stock: 11 },
-    { id: 3, nombre: "Piano bar", año: 1984, portada: "/img/piano bar.jpg", precio: 700, stock: 6 },
-    { id: 4, nombre: "Parte de la religion", año: 1987, portada: "/img/parte de la religion.jpg", precio: 650, stock: 3 },
-    { id: 5, nombre: "Como conseguir chicas", año: 1989, portada: "/img/como conseguir chicas.jpg", precio: 700, stock: 3 },
-]
 
-
-const Item = () => {
-    return (
-        items.map((elemento, indice) => {
-            return (
-                <div key={indice} className="tarjeta-producto">
-                    <img src={elemento.portada} alt={elemento.nombre} className="img-producto" />
-                    <div className="info-producto">
-                        <p className="nombre-producto">{elemento.nombre}</p>
-                        <p className="año-producto">Año: {elemento.año}</p>
-                        <p className="precio-producto">Precio: ${elemento.precio}</p>
-                        <ItemCount stock={elemento.stock} initial={1} />
-                    </div>
-                </div>
-            )
-        }
-        )
-    )
+const Item = ({elemento}) => {
+return(
+    <article>
+        <h2>{elemento.nombre}</h2>
+        <img src={elemento.portada} alt={elemento.nombre} className="img-producto" />
+        <p className="precio-producto">Precio: ${elemento.precio}</p>
+        <ItemCount stock={elemento.stock} initial={1} />
+    </article>
+)
 }
 
 export default Item
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,5 +1,16 @@
 import Item from "./Item"
 import { useEffect, useState } from "react"
+import ItemCount from "./ItemCount"
+
+const items = [
+    { id: 1, nombre: "Yendo de la cama al living", año: 1982, portada: "/img/yendo de la cama al living.jpg", precio: 800, stock: 5 },
+    { id: 2, nombre: "Clics modernos", año: 1983, portada: "/img/clics modernos.jpg", precio: 1000, stock: 11 },
+    { id: 3, nombre: "Piano bar", año: 1984, portada: "/img/piano bar.jpg", precio: 700, stock: 6 },
+    { id: 4, nombre: "Parte de la religion", año: 1987, portada: "/img/parte de la religion.jpg", precio: 650, stock: 3 },
+    { id: 5, nombre: "Como conseguir chicas", año: 1989, portada: "/img/como conseguir chicas.jpg", precio: 700, stock: 3 },
+]
+
+
 
 const ItemList = () => {
 
@@ -9,7 +20,7 @@ const ItemList = () => {
     useEffect(() => {
         const promesa = new Promise((res, rej) => {
             setTimeout(() => {
-                res(<Item />)
+                res(items)
             }, 2000)
         })
 
@@ -23,11 +34,30 @@ const ItemList = () => {
 
 
 
-    return (
-        <div className="contenedor-producto">
-            {loading ? <p>Cargando...</p> : <Item />}
-        </div>
-    )
+    if (loading) {
+        return (
+            <p> Cargando...</p>
+        )
+    } else {
+        return (
+            productos.map((elemento, indice) => {
+                return (
+                    <div key={indice} className="tarjeta-producto">
+                        <img src={elemento.portada} alt={elemento.nombre} className="img-producto" />
+                        <div className="info-producto">
+                            <p className="nombre-producto">{elemento.nombre}</p>
+                            <p className="año-producto">Año: {elemento.año}</p>
+                            <p className="precio-producto">Precio: ${elemento.precio}</p>
+                            <ItemCount stock={elemento.stock} initial={1} />
+                        </div>
+                    </div>
+
+                )
+            }
+
+            )
+        )
+    }
 }
 
 
